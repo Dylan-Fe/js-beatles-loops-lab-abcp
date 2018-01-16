6ff5d2b1c74be7d959919a3e46e514354c7255f9
Dylan Ferrara <dylan.ferrara@gmail.com>
2018-01-16 06:01:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(mus, inst) {
   var newArr = [];
   
   for(let i = 0; i < mus.length; ++i) {
-    var newStr = `${mus[i]} plays ${inst[0]}`;
+    var newStr = `${mus[i]} plays ${inst[i]}`;
     newArr.push(newStr);
   }
   return newArr;
